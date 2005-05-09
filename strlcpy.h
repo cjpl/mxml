9de8c4ac0b015aecd2a905d1b671a272e498b5a1
Stefan Ritt <stefan.ritt@psi.ch>
2005-05-09 09:12:06 +0000

    Initial revision

---
+++ strlcpy.h
@@ -0,0 +1,23 @@
+/********************************************************************\
+
+   Name:         strlcpy.h
+   Created by:   Stefan Ritt
+
+   Contents:     Header file for strlcpy.c
+
+   $Log$
+   Revision 1.1  2005/05/09 09:12:06  ritt
+   Initial revision
+
+\********************************************************************/
+
+#ifndef EXPRT
+#if defined(EXPORT_DLL)
+#define EXPRT __declspec(dllexport)
+#else
+#define EXPRT
+#endif
+#endif
+
+size_t EXPRT strlcpy(char *dst, const char *src, size_t size);
+size_t EXPRT strlcat(char *dst, const char *src, size_t size);
