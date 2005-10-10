409d9682fb86ec1f0f592fb9043a12421e791ff5
ritt <ritt@e3eee304-8902-0410-aafd-c07b25ac10c4>
2005-10-10 06:53:16 +0000

    Removed stripping of \" in mxml_decode.
    git-svn-id: svn+ssh://savannah.psi.ch/afs/psi.ch/project/meg/svn/mxml/trunk@30 e3eee304-8902-0410-aafd-c07b25ac10c4

--- mxml.c
+++ mxml.c
@@ -270,11 +270,6 @@ void mxml_decode(char *str)
          strcpy(p, p+5);
       }
    }
-
-   if (str[0] == '\"' && str[strlen(str)-1] == '\"') {
-      strcpy(str, str+1);
-      str[strlen(str)-1] = 0;
-   }
 }
 
 /*------------------------------------------------------------------*/
